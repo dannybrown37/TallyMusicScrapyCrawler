4a7350d169ef3cbe5ddb1cc91e9fad7fda579b6f
Danny Brown <dannybrown37@gmail.com>
2018-10-27 10:41:28 -0400

    added auto-deleting of blank raw data files for value

--- crawl_all.py
+++ crawl_all.py
@@ -8,6 +8,7 @@ setting = get_project_settings()
 process = CrawlerProcess(setting)
 
 output_files = [
+    "bbcoutput.json",
     "bluetavernoutput.json",
     "cduoutput.json",
     "cocaoutput.json",
@@ -19,7 +20,7 @@ output_files = [
     "wilburyoutput.json",
 ]
 
-# If output file exists, delete it
+# If output file exists, delete it. Otherwise data will just be concatenated
 for output_file in output_files:
     try:
         os.remove("output/" + output_file)
@@ -32,6 +33,22 @@ for spider_name in process.spiders.list():
     process.crawl(spider_name, query="dvh") #query dvh is custom argument used in your scrapy
 
 process.start()
+
+# Let's remove empty files
+def check_for_empty_file(fpath):
+    try:
+        return os.stat(fpath).st_size == 0
+    except WindowsError:
+        pass
+for output_file in output_files:
+    output_path = os.path.abspath("output/" + output_file)
+    if check_for_empty_file(output_path):
+        print output_path
+        try:
+            os.remove(output_path)
+        except OSError:
+            pass
+
 
 # Filter and combine the files
 filter_and_combine.combine_json_files()
